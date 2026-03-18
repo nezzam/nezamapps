5e93ad232cbcd34b631e34f6a72f47a2e97f26bb
aymangam1 <aymanfm3001@gmail.com>
2026-03-18 17:49:05 +0200

    UpLoad

--- nz_transfer_line_destination/__manifest__.py
+++ nz_transfer_line_destination/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Stock Transfer Line Destination',
-    'version': '15.0.1.0',
+    'version': '15.0.1.0.1',
     'summary': 'Set destination location per operation line in transfers',
     'category': 'Inventory/Inventory',
     'depends': ['stock','account'],
