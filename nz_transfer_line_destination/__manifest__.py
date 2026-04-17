fb5f16eaa39614558c54243053764dd5f53c165e
aymangam1 <aymanfm3001@gmail.com>
2026-04-18 00:32:15 +0200

    Update nz_transfer_line_destination Version

--- nz_transfer_line_destination/__manifest__.py
+++ nz_transfer_line_destination/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Stock Transfer Line Destination',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'summary': 'Set destination location per operation line in transfers',
     'author': 'Nezam',
     'company': 'Nezam',
