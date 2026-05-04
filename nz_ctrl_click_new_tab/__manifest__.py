fb03ac4558f85ca11bd947779ad088b84c11fbee
aymangam1 <aymanfm3001@gmail.com>
2026-05-05 01:58:00 +0200

    Update manifest with new version and metadata
    Updated version number and added company, maintainer, website, and support information.

--- nz_ctrl_click_new_tab/__manifest__.py
+++ nz_ctrl_click_new_tab/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'CTRL + Click to Open New Tab',
-    'version': '19.0.1.0.0',
+    'version': '18.0.1.0.0',
     'summary': 'Hold CTRL (or Cmd on macOS) and click any record to open it in a new browser tab.',
     'description': '''
         Enables CTRL+Click (Cmd+Click on macOS) to open Odoo backend records
@@ -10,13 +10,16 @@
     ''',
     'category': 'Technical',
     'author': 'Nezam',
-    'website': '',
+    'company': 'Nezam',
+    'maintainer': 'Nezam',
+    'website': 'https://www.nezam.co',
+    'support': 'support@nezam.co',
+    'images': ['static/description/banner.png'],
     'license': 'LGPL-3',
     'depends': ['web'],
     'data': [],
     'assets': {
         'web.assets_backend': [
-            'nz_ctrl_click_new_tab/static/src/xml/ctrl_click_patch.xml',
             'nz_ctrl_click_new_tab/static/src/js/ctrl_click_new_tab.js',
         ],
     },
